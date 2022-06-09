e061d8f58aff76a6df3d720b4d01b0ac0600ee69
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2022-06-09 18:33:53 +0300

    часть обучения по передачи состояния наверх

--- step-by-step-tutorial/src/index.js
+++ step-by-step-tutorial/src/index.js
@@ -1296,8 +1296,54 @@ root.render(<Reservation />);
     Если value указано, но input редактируемый, то можно случайно выставить value в undefined или null 
 */
 
+// zakommentirovala poka
+/*
 root.render(<input value="hi" />);
 
 setTimeout(function() {
     root.render(<input value={null} />);
 }, 5000);
+*/
+
+/************************** Продвижение состояния наверх ******************************/
+
+/*
+    Часто, нескольким компонентам необходимо отображать те же самые изменяющиеся данные.
+    Рекомендуется выносить совместное состояние наверх до ближайшего общего родителя 
+*/
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
+
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render() {
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>Enter temperature in Celsius</legend>
+                <input 
+                    value={temperature}  
+                    onChange={this.handleChange}
+                />
+                <BoilingVerdict
+                    celsius={parseFloat(temperature)} />
+            </fieldset>
+        );
+    }
+}
+
+root.render(<Calculator />);
