40049f2c56c49bc745eb71773840103d47aee470
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2022-06-10 20:04:17 +0300

    Жалкие попытки поиграться с асинхронным импортом

--- code-splitting/src/index.js
+++ code-splitting/src/index.js
@@ -7,6 +7,12 @@ import { add } from './utilities/math.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+class TestImport extends React.Component {
+    render() {
+        return <h1>Hello, kitty, konichiwa!</h1>;
+    }
+}
+
 class App extends React.Component {
     render() {
         return (
@@ -14,6 +20,7 @@ class App extends React.Component {
                 <p>Using imported js-function: result is {add(5, 10)}</p>
                 <AdditionElementFunction a={5} b={10} />
                 <AdditionElementClass a={30} b={20} />
+                <TestImport />
             </div>
         )
     }
