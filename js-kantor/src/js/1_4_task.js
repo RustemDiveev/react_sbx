9ec5d15b1780e95b8e421cfdbb22cd2e7a95d209
Rustem Diveev <rustem.diveev@gmail.com>
2022-06-18 16:07:42 +0300

    Переменные и типы данных

---
+++ js-kantor/src/js/1_4_task.js
@@ -0,0 +1,14 @@
+/*
+    1. Объявите 2 переменные - admin и name 
+    2. Записать в name строку "Василий"
+    3. Скопироать значение из name в admin 
+    4. Вывести admin
+*/
+
+"use strict";
+
+function Solution() {
+    const name = "Василий";
+    const admin = name;
+    alert(admin);
+}
