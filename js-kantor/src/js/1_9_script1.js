6a5e46a2028fd6e5cc320d80fab5be2746a329a0
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2022-06-23 15:31:56 +0300

    Условные операторы

--- js-kantor/src/js/1_9_script1.js
+++ js-kantor/src/js/1_9_script1.js
@@ -1,12 +1,24 @@
 function task2() {
-    let result = prompt("Кто мы?");
-    let div = document.getElementById("TASK2");
+    const result = prompt("Кто мы?");
+    const div = document.getElementById("TASK2");
     div.innerText = (result == "Вкусовые сосочки") ? "Хотим чистую линию!" : "Ты в пиве!";
 }
 
 function task3() {
-    let result = prompt("Введи чиселку, браток!");
+    const result = prompt("Введи чиселку, браток!");
     (result > 0) ? alert(1) : 
         (result < 0) ? alert(-1) :
         (result == 0) ? alert(0) : alert("Ты ввел какую-то дичь!");
+}
+
+function task4() {
+    function ask_password() {
+        const result = prompt("Your password, sir?");
+        (result === "Black Lord") ? alert("Welcome") : 
+            (result === null) ? alert("Auth was cancelled") : alert("No entry!");
+    }
+
+    const result = prompt("Your login, sir?", );
+    (result === "Admin") ? ask_password() : 
+        (result === null) ? alert("Auth was cancelled") : alert("Who are you?");
 }
